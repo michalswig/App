44dad50f1ede6448bfe797361fba770a45592fbc
Michał Świgost <mike@data.pl>
2021-04-14 11:52:22 +0200

    tasks 22.2-3

---
+++ src/main/java/com/crud/tasks/domain/TrelloBoardDto.java
@@ -0,0 +1,27 @@
+package com.crud.tasks.domain;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class TrelloBoardDto {
+
+    @JsonProperty("id")
+    private String id;
+
+    @JsonProperty("name")
+    private String name;
+
+    @JsonProperty("lists")
+    private List<TrelloListDto> lists;
+
+    public TrelloBoardDto(String test_id, String test_board, ArrayList<Object> objects) {
+    }
+}
