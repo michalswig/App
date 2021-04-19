df6dd14e06e5ab3c5500530e150a15a36f799f56
Michał Świgost <mike@data.pl>
2021-04-19 08:57:58 +0200

    23.2Task

--- src/main/java/com/crud/tasks/TasksApplication.java
+++ src/main/java/com/crud/tasks/TasksApplication.java
@@ -18,14 +18,14 @@ public class TasksApplication {
     public static void main(String[] args) {
 
 //		TaskDto taskDto = new TaskDto(
-//			(long)1,
-//			"Test title",
-//			"I want to be a coder!");
-//
-//		Long id = taskDto.getId();
-//		String title = taskDto.getTitle();
-//		String content = taskDto.getContent();
-//		System.out.println(id + " " + title + " " + content);
+////			(long)1,
+////			"Test title",
+////			"I want to be a coder!");
+////
+////		Long id = taskDto.getId();
+////		String title = taskDto.getTitle();
+////		String content = taskDto.getContent();
+////		System.out.println(id + " " + title + " " + content);
 
         SpringApplication.run(TasksApplication.class, args);
     }
